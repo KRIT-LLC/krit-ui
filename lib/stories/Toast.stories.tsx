6af99af39171ad5dc24d844d8a017b0ee194a79b
Vitaly Gashkov <vitalygashkov@vk.com>
2024-06-06 13:22:24 +0500

    feat: добавить создание всплывающего уведомления

--- lib/stories/Toast.stories.tsx
+++ lib/stories/Toast.stories.tsx
@@ -1,6 +1,7 @@
-import type {Meta, StoryFn} from '@storybook/react';
-import {Toaster} from '@/components/ui/toaster';
-import {Button} from '../components/ui/button';
+import type { Meta, StoryFn } from '@storybook/react';
+import { Toaster } from '@/components/ui/toaster';
+import { Button } from '@/components/ui/button';
+import { useToast } from '@/hooks/useToast';
 
 export default {
   title: 'Toaster',
@@ -9,13 +10,18 @@ export default {
     layout: 'centered',
   },
   tags: ['autodocs'],
-  argTypes: {}
+  argTypes: {},
 } satisfies Meta<typeof Toaster>;
 
-
 export const Demo: StoryFn = (args) => {
-  return <div>
-    <Button>Вывести оповещение</Button>
-    <Toaster {...args}/>
-  </div>;
+  const { toast } = useToast();
+
+  const makeToast = () => toast({ title: 'Toast Title', description: 'Toast Description' });
+
+  return (
+    <>
+      <Button onClick={makeToast}>Make toast</Button>
+      <Toaster {...args} />
+    </>
+  );
 };
