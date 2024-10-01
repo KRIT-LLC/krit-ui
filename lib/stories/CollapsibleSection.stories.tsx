6dd77b419348cfe63eec046f9ab7ec20e6889e3c
pitaev.ev <evgeny.pitaev@krit.pro>
2024-10-01 20:57:30 +0500

    Исправлен запуск storybook

--- lib/stories/CollapsibleSection.stories.tsx
+++
@@ -1,8 +0,0 @@
-import type { StoryFn } from '@storybook/react';
-import { CollapsibleSection } from '@/components/ui/collapsible-section';
-
-const Demo: StoryFn = (args) => {
-  return <CollapsibleSection title={''} {...args} />;
-};
-
-export default Demo;
