c9e45801617f09797c2987c5dc674ebd797e568f
Aleksei Malyshev <79637346025@ya.ru>
2024-10-22 20:03:44 +0700

    add  text value to InputFile

--- lib/components/ui/file-input.tsx
+++ lib/components/ui/file-input.tsx
@@ -6,15 +6,19 @@ import { Input } from './input';
 import { validateFile } from '@/lib/file';
 
 export interface FileInputProps extends React.InputHTMLAttributes<HTMLInputElement> {
-  onAdd: (file: File, error?: {fileSizeError?: string, fileTypeError?: string}) => void;
+  onAdd: (file: File, error?: { fileSizeError?: string; fileTypeError?: string }) => void;
   onFileRemove?: (onConfirmCallback?: () => void) => void;
   onClick?: () => void;
   error?: string | boolean;
   maxFileSize: number;
+  textValue?: string;
 }
 
 const FileInput = React.forwardRef<HTMLInputElement, FileInputProps>(
-  ({ className, onAdd, onClick, onFileRemove, error, accept, maxFileSize, ...props }, ref) => {
+  (
+    { className, onAdd, onClick, onFileRemove, error, accept, maxFileSize, textValue, ...props },
+    ref,
+  ) => {
     const [fileName, setFileName] = React.useState('');
 
     const handleInputChange = async (files: FileList) => {
@@ -24,7 +28,10 @@ const FileInput = React.forwardRef<HTMLInputElement, FileInputProps>(
         setFileName(files[0].name);
         onAdd(files[0]);
       } else {
-        onAdd(files[0], { fileTypeError: result.fileTypeError, fileSizeError: result.fileSizeError });
+        onAdd(files[0], {
+          fileTypeError: result.fileTypeError,
+          fileSizeError: result.fileSizeError,
+        });
       }
     };
 
@@ -66,7 +73,7 @@ const FileInput = React.forwardRef<HTMLInputElement, FileInputProps>(
             onChange={e => e.target.files?.length && handleInputChange(e.target.files)}
           />
           <Input
-            defaultValue={fileName}
+            defaultValue={fileName || textValue}
             style={{ pointerEvents: 'none', background: 'white' }}
             placeholder={props.placeholder}
             error={error}
