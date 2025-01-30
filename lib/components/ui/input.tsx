b31752c7549f278151b36534d6ef6e87a14f5aa7
Aleksei Malyshev <79637346025@ya.ru>
2025-01-30 10:37:57 +0700

    add count prop to input

--- lib/components/ui/input.tsx
+++ lib/components/ui/input.tsx
@@ -1,20 +1,26 @@
 import * as React from 'react';
 import { cn } from '@/utils';
-import ViewOutline from '@/assets/view_outline.svg?react';
 import HideOutline from '@/assets/hide_outline.svg?react';
 import Search from '@/assets/search.svg?react';
+import ViewOutline from '@/assets/view_outline.svg?react';
 
 export interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
   error?: string | boolean;
   asSearch?: boolean;
   rightIcon?: React.ReactNode;
+  withCount?: boolean;
   onEnter?: (value: string) => void;
 }
 
 const Input = React.forwardRef<HTMLInputElement, InputProps>(
-  ({ className, type, error, asSearch, rightIcon, onEnter, ...props }, ref) => {
+  ({ className, type, error, asSearch, rightIcon, withCount, onEnter, ...props }, ref) => {
     const [passwordVisible, setPasswordVisible] = React.useState(false);
-    const shouldWrapWithRelative = asSearch || type === 'password' || rightIcon;
+    const [valueLength, setValueLength] = React.useState(0);
+    const shouldWrapWithRelative = asSearch || type === 'password' || rightIcon || withCount;
+    const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+      setValueLength(e.target.value.length);
+      props.onChange?.(e);
+    };
 
     const input = (
       <>
@@ -24,7 +30,7 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(
             'flex h-9 w-full rounded-lg border border-line-primary bg-[transparent] blur-none px-3 pr-[0.95rem] py-2 text-sm tracking-[0.1px] leading-5 transition-colors duration-300 ease-in-out ring-offset-background file:border-0 file:bg-[transparent] file:text-sm font-normal placeholder:text-foreground-tertiary focus-visible:outline-none focus-visible:border-line-focused disabled:cursor-not-allowed disabled:opacity-50 truncate line-clamp-1',
             error ? 'border-line-error focus-visible:border-line-error' : '',
             !shouldWrapWithRelative && className,
-            rightIcon && 'pr-14',
+            (rightIcon || withCount) && 'pr-14',
             props.readOnly && 'cursor-not-allowed pointer-events-none opacity-95',
           )}
           ref={ref}
@@ -33,9 +39,18 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(
             if (e.key === 'Enter') onEnter?.((e.target as HTMLInputElement).value);
           }}
           {...props}
+          onChange={onChange}
         />
         {rightIcon && (
           <span className='absolute right-1.5 top-1/2 -translate-y-1/2'>{rightIcon}</span>
+        )}
+        {withCount && (
+          <span
+            className='absolute right-1.5 top-1/2 -translate-y-1/2
+            text-foreground-tertiary text-xs'
+          >
+            {valueLength}
+          </span>
         )}
         {asSearch && <Search className='absolute top-[10px] right-3 text-icon-fade-contrast' />}
         {type === 'password' && (
