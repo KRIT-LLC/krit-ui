c3e03d49fb9b57d7b2dab03b7e5df87dad7e19bd
pitaev.ev <evgeny.pitaev@krit.pro>
2025-03-03 17:09:49 +0500

    Добавлен компонент SegmentedControl

---
+++ lib/stories/SegmentedControl.stories.tsx
@@ -0,0 +1,32 @@
+import { useState } from 'react';
+import type { Meta, StoryObj } from '@storybook/react';
+import { CheckIcon, XIcon } from 'lucide-react';
+import { SegmentedControl } from '@/components/ui/segmented-control.tsx';
+
+const meta = {
+  title: 'SegmentedControl',
+  component: SegmentedControl,
+  parameters: {
+    layout: 'centered',
+  },
+  tags: ['autodocs'],
+} satisfies Meta<typeof SegmentedControl>;
+
+export default meta;
+
+export const Demo: StoryObj<typeof meta> = {
+  render: args => {
+    const [value, setValue] = useState(args.defaultValue);
+
+    return (
+      <SegmentedControl {...args} defaultValue={value} onClick={newValue => setValue(newValue)} />
+    );
+  },
+  args: {
+    defaultValue: 'active',
+    options: [
+      { label: 'Active', value: 'active', icon: <CheckIcon /> },
+      { label: 'Inactive', value: 'inactive', icon: <XIcon /> },
+    ],
+  },
+};
