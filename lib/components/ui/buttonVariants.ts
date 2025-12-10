8a0365a57987061df3e7f435d4c179d5d98199c6
Aleksei <a.s.malyshev.gm@gmail.com>
2025-12-10 20:24:28 +0700

    fix button and tabs icons

--- lib/components/ui/buttonVariants.ts
+++ lib/components/ui/buttonVariants.ts
@@ -7,23 +7,23 @@ export const buttonVariants = cva(
       variant: {
         // Соответствует старым вариантам с серым фоном: default, secondary-contrast, contrast-fade, grey, grey-primary, secondary
         'fade-contrast-filled':
-          'bg-background-contrast-fade text-foreground-primary hover:bg-background-contrast-fade-hover disabled:bg-background-contrast-fade-disabled disabled:text-foreground-primary-disabled',
+          'bg-background-contrast-fade text-foreground-primary hover:bg-background-contrast-fade-hover disabled:bg-background-contrast-fade-disabled disabled:text-foreground-primary-disabled [&_svg]:text-icon-contrast disabled:[&_svg]:text-icon-contrast-disabled',
         // Соответствует старым вариантам прозрачным с бордерами: outline, secondary-outline
         'fade-contrast-outlined':
-          'border border-line-primary bg-transparent text-foreground-primary hover:bg-background-primary-hover hover:border-line-primary disabled:border-line-primary-disabled disabled:text-foreground-primary-disabled',
+          'border border-line-primary bg-transparent text-foreground-primary hover:bg-background-primary-hover hover:border-line-primary disabled:border-line-primary-disabled disabled:text-foreground-primary-disabled [&_svg]:text-icon-contrast disabled:[&_svg]:text-icon-contrast-disabled',
         // Соответствует старым вариантам прозрачным: ghost, link, light
         'fade-contrast-transparent':
-          'bg-transparent text-foreground-primary hover:bg-background-primary-hover disabled:text-foreground-primary-disabled',
+          'bg-transparent text-foreground-primary hover:bg-background-primary-hover disabled:text-foreground-primary-disabled [&_svg]:text-icon-contrast disabled:[&_svg]:text-icon-contrast-disabled',
         // Соответствует старым вариантам с цветным фоном (не серым, не красным): primary, contrast, accent, success, success-primary, pale, pale-primary, purple
         'theme-filled':
-          'bg-background-theme text-foreground-on-contrast hover:bg-background-theme-hover disabled:bg-background-contrast-fade-disabled disabled:text-foreground-primary-disabled',
+          'bg-background-theme text-foreground-on-contrast hover:bg-background-theme-hover disabled:bg-background-contrast-fade-disabled disabled:text-foreground-primary-disabled [&_svg]:text-icon-on-contrast disabled:[&_svg]:text-icon-contrast-disabled',
         // Соответствует старым вариантам с красным фоном: destructive, destructive-primary
         'warning-filled':
-          'bg-background-error text-foreground-on-contrast hover:bg-background-error-hover disabled:bg-background-contrast-fade-disabled disabled:text-foreground-primary-disabled',
+          'bg-background-error text-foreground-on-contrast hover:bg-background-error-hover disabled:bg-background-contrast-fade-disabled disabled:text-foreground-primary-disabled [&_svg]:text-icon-on-contrast disabled:[&_svg]:text-icon-contrast-disabled',
         'nav-item':
           'hover:bg-background-contrast-fade-selected/30 text-foreground-tertiary hover:text-foreground-white',
         'nav-item-selected':
-          'bg-background-contrast-fade-selected/30  text-foreground-sidebar-active',
+          'bg-background-contrast-fade-selected/30 text-foreground-sidebar-active',
       },
       size: {
         default: 'h-9 px-4 py-2',
