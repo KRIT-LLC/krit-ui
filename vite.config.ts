5580bac9336de14311803f34e4f1256006a3c9a8
pitaev.ev <evgeny.pitaev@krit.pro>
2025-12-11 09:20:54 +0500

    Исключить Stories из конечной сборки

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,22 @@
+import react from '@vitejs/plugin-react';
 import { resolve } from 'node:path';
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import svgr from 'vite-plugin-svgr';
 import dts from 'vite-plugin-dts';
+import svgr from 'vite-plugin-svgr';
 
 export default defineConfig({
-  plugins: [react(), svgr(), dts({ include: ['lib'], insertTypesEntry: true })],
+  plugins: [
+    react(),
+    svgr(),
+    dts({ include: ['lib'], exclude: ['lib/stories/**'], insertTypesEntry: true }),
+  ],
   resolve: { alias: { '@': resolve(__dirname, './lib') } },
   build: {
     lib: {
       entry: resolve(__dirname, 'lib/main.ts'),
       name: 'krit-ui',
       fileName: 'krit-ui',
-      formats: ["es", "umd"],
+      formats: ['es', 'umd'],
     },
     rollupOptions: {
       external: ['react', 'react-dom', 'react/jsx-runtime', 'tailwindcss'],
